bdd9d1a532c215bc25943418663816901e7f60da
nenadV91 <nenadwebdevelop@gmail.com>
2022-10-06 20:18:01 +0200

    Update for correct format

--- script.js
+++ script.js
@@ -41,6 +41,47 @@ function createSortMap(tokens) {
   }, {});
 }
 
+function createFinalResult(tokens, version) {
+  return JSON.stringify(
+    {
+      name: "CoW Protocol",
+      timestamp: new Date().toISOString(),
+      version: Object.assign(
+        {
+          major: 0,
+          minor: 1,
+          patch: 7,
+        },
+        version
+      ),
+      logoURI:
+        "https://gateway.pinata.cloud/ipfs/QmQxFkVZzXFyWf73rcFwNPaEqG5hBwYXrwrBEX3aWJrn2r/cowprotocol.png",
+      keywords: ["default", "list", "cowprotocol"],
+      tokens: tokens,
+    },
+    0,
+    4
+  );
+}
+
+async function combineWithCow(tokens) {
+  const output = [...tokens];
+
+  const cowTokens = await fetchCowList();
+
+  cowTokens.tokens.forEach((t) => {
+    const match = output.find(
+      (c) => c.symbol.toLowerCase() === t.symbol.toLowerCase()
+    );
+
+    if (!match) {
+      output.push(t);
+    }
+  });
+
+  return output;
+}
+
 async function dlTokens() {
   try {
     const page = 1;
@@ -64,12 +105,14 @@ async function dlTokens() {
   }
 }
 
-async function combineTokens() {
+async function combineTokens(version) {
   const output = [];
 
   const jsonsInDir = fs
     .readdirSync("./results")
-    .filter((file) => path.extname(file) === ".json");
+    .filter(
+      (file) => path.extname(file) === ".json" && file.startsWith("tokens")
+    );
 
   jsonsInDir.forEach((file) => {
     const fileData = fs.readFileSync(path.join("./results", file));
@@ -77,8 +120,10 @@ async function combineTokens() {
     output.push(...json);
   });
 
-  dlFile("combined.json", JSON.stringify(output, 0, 4));
+  const withCow = await combineWithCow(output);
+
+  dlFile("combined.json", createFinalResult(withCow, version));
 }
 
 // dlTokens();
-combineTokens();
+combineTokens({ patch: 8 });
